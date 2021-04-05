2fb2b6094d676933a90e581c4acdf204cce11e95
brandonpollett <brpoll@microsoft.com>
2021-04-05 10:23:09 -0700

    Update to support the keyvault store.

--- DicomProduction/Program.cs
+++ DicomProduction/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 
 namespace DicomProduction
@@ -7,14 +12,25 @@ namespace DicomProduction
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            IWebHost host = WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, builder) =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    IConfigurationRoot builtConfig = builder.Build();
+
+                    var keyVaultEndpoint = builtConfig["KeyVault:Endpoint"];
+                    if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                    {
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                        builder.AddAzureKeyVault(keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                    }
+
+                })
+                .ConfigureKestrel(option => option.Limits.MaxRequestBodySize = int.MaxValue) // When hosted on Kestrel, it's allowed to upload >2GB file, set to 2GB by default
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
     }
 }
